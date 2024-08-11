002625d4ccff4de98fd10c5ec22968fdbca2dbbb
rustamrustamv <118830149+rustamrustamv@users.noreply.github.com>
2024-08-11 20:48:21 +0200

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@
 //
 class Hello {
   public static void main(String[] args) {
-    System.out.println("Welcome to Jenkins Online Course! , Rustam");
+    System.out.println("Welcome to Jenkins Online Course! , Amina");
   }
 }
